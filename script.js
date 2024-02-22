95e2fbb64af942912b812378aa58b931b015df18
LenaSemenova <134925614+LenaSemenova@users.noreply.github.com>
2024-02-22 20:05:16 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ let secondsInfo = document.querySelector(".secondsInfo");
 let alertMinutes = document.querySelector("#alertMinutes");
 let alertSeconds = document.querySelector("#alertSeconds");
 let allBtns = document.querySelectorAll("button");
+let btnStart = document.querySelector("#start");
+let btnStop = document.querySelector("#stop");
+let btnReset = document.querySelector("#reset");
+
 
 
 //Functions
@@ -23,11 +27,8 @@ function amountOfMinutes () {
 }
 function inputFieldsControl () {
 	let secondsControl = amountOfSeconds();
-	console.log(secondsControl);
 	let minutesControl = amountOfMinutes();
-	console.log(minutesControl);
 	let totalControl = ((minutesControl * 60) + secondsControl) / 60;
-	console.log(totalControl);
 	if (secondsControl > 60) {
 		alertSeconds.classList.remove("hidden");
 		allBtns.forEach(button => {
@@ -58,7 +59,7 @@ let intervalID;
 let intervalID_array = [];
 let progressStartValue;
 
-function settingAnInterval (valueSec, valueMin, totalTime) {
+function settingAnInterval (valueSec, valueMin) {
 intervalID = setInterval(() => {
 	intervalID_array.push(intervalID);
 	if(valueSec === 0 && valueMin !==0) {
@@ -87,15 +88,12 @@ intervalID = setInterval(() => {
 	}
   if (valueSec > 0 && valueMin !== undefined) {
   circularProgress.style.background = `conic-gradient(pink ${valueSec * 6}deg, #ededed 0deg)`;
-  console.log("valueSec: ", valueSec);
   valueSec = valueSec - 1;
   
   if (valueSec === 0 && valueMin > 0) { 
   circularProgress.style.background = `conic-gradient(pink ${(valueSec + 1) * 6}deg, #ededed 0deg)`;
 	valueMin = valueMin -1;
 	valueSec = 60;
-//circularProgress.style.background = `conic-gradient(pink ${valueSec * 6}deg, #ededed 0deg)`;
-//console.log(`conic-gradient(pink ${(valueSec) * 6}deg, #ededed 0deg)`);
     
   }
 } else {
@@ -109,20 +107,19 @@ circularProgress.style.background = `conic-gradient(pink 0deg, #ededed 0deg)`;
 function countDown(){
 let valueSec = amountOfSeconds();
 let valueMin = amountOfMinutes();
-let totalTime = (valueMin * 60) + valueSec;
 if (!intervalID) {
-	settingAnInterval(valueSec, valueMin, totalTime);
+	settingAnInterval(valueSec, valueMin);
 }
 }
 
 function stopContinue () {
-  if (document.querySelector("#stop").innerHTML === `Stop\n\t<span class="glyphicon glyphicon-pause"></span>`) {
-    document.querySelector("#stop").innerHTML = `Continue\n\t<span class="glyphicon glyphicon-pause"></span>`;
+  if (btnStop.innerHTML === `Stop\n\t<span class="glyphicon glyphicon-pause"></span>`) {
+    btnStop.innerHTML = `Continue\n\t<span class="glyphicon glyphicon-pause"></span>`;
     for (let i = 0; i< intervalID_array.length; i++) {
       clearInterval(intervalID_array[i]);
     }
-  } else if (document.querySelector("#stop").innerHTML === `Continue\n\t<span class="glyphicon glyphicon-pause"></span>`) {
-  document.querySelector("#stop").innerHTML = `Stop\n\t<span class="glyphicon glyphicon-pause"></span>`;
+  } else if (btnStop.innerHTML === `Continue\n\t<span class="glyphicon glyphicon-pause"></span>`) {
+  btnStop.innerHTML = `Stop\n\t<span class="glyphicon glyphicon-pause"></span>`;
   let valueSec = Number(secondsInfo.textContent);
   let valueMin = Number(minutesInfo.textContent);
   settingAnInterval(valueSec, valueMin);
@@ -135,16 +132,16 @@ function reset () {
   secondsInput.value = "";
   minutesInfo.textContent = "00";
   secondsInfo.textContent = "00";
-  document.querySelector("#stop").textContent = "Stop";
+  btnStop.innerHTML = `Stop\n\t<span class="glyphicon glyphicon-pause"></span>`;
   circularProgress.style.background = `conic-gradient(pink 0deg, #ededed 0deg)`;
   clearInterval(intervalID);
   intervalID = null;
 }
 
 function init() {
-  document.querySelector("#start").addEventListener("click", countDown);
-  document.querySelector("#stop").addEventListener("click", stopContinue);
-  document.querySelector("#reset").addEventListener("click", reset);
+  btnStart.addEventListener("click", countDown);
+  btnStop.addEventListener("click", stopContinue);
+  btnReset.addEventListener("click", reset);
   minutesInput.addEventListener("blur", inputFieldsControl);
   secondsInput.addEventListener("blur", inputFieldsControl);
    setTimeout(() => {
